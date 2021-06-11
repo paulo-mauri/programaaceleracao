2ca172bd151e0c001d073103ace8250a5470192c
Paulo Mauri <paulo.mauri@gmail.com>
2021-06-10 21:26:19 -0300

    Angular Exercicio

--- Projetos/gatitobook/src/app/app.module.ts
+++ Projetos/gatitobook/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
