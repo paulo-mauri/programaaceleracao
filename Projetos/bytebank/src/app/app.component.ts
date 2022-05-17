afdc299c714e93b35fb6f41b07a95b72ec97bca0
Paulo Mauri <paulo.mauri@gmail.com>
2022-05-16 21:28:31 -0300

    inicial bytebank

--- Projetos/bytebank/src/app/app.component.ts
+++ Projetos/bytebank/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bytebank';
 
-  constructor(private service: TransferenciaService) {}
+  transferir($event) {
+    console.log($event);
+  }
 }
--- Projetos/bytebank/src/app/app.module.ts
+++ Projetos/bytebank/src/app/app.module.ts
@@ -1,33 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import { FormsModule } from '@angular/forms';
-import { ExtratoComponent } from './extrato/extrato.component';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app.routing.module';
-
-registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent,
-    ExtratoComponent
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [
-    {provide: LOCALE_ID, useValue: 'pt'},
-    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
