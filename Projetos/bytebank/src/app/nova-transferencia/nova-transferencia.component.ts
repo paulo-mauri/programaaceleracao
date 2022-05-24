31b997e7dcb9680ecad660acb532b89e61433002
Paulo Mauri <paulo.mauri@gmail.com>
2022-05-24 20:14:07 -0300

    angular-parte-1

--- Projetos/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ Projetos/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
+import { TranseferenciaService } from './../services/transeferencia.service';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,16 +11,40 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 export class NovaTransferenciaComponent {
 
-  @Output() aoTransferir = new EventEmitter<any>();
+    //@Output() aoTransferir = new EventEmitter<any>();
 
-  valor: number ;
-  destino: number ;
+    valor: number ;
+    destino: number ;
 
-  transferir() {
-    console.log('Solicitado nova transferência');
-    // console.log('Valor: ', this.valor);
-    // console.log('Destino', this.destino);
-    const valorEmitir = { valor: this.valor, destino: this.destino };
-    this.aoTransferir.emit(valorEmitir);
-  }
+    constructor(private transferenciaService: TranseferenciaService,
+                private router: Router) {
+
+    }
+
+    transferir() {
+        console.log('Solicitado nova transferência');
+
+        // console.log('Valor: ', this.valor);
+        // console.log('Destino', this.destino);
+        //const valorEmitir = { valor: this.valor, destino: this.destino };
+        // this.aoTransferir.emit(valorEmitir);
+
+        const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
+
+        this.transferenciaService.adicionar(valorEmitir)
+            .subscribe(resultado => {
+                console.log(resultado);
+
+                this.limparCampos();
+
+                this.router.navigateByUrl('extrato');
+            },
+            (error) => console.log(error)
+        );
+    }
+
+    limparCampos() {
+        this.valor = 0;
+        this.destino = 0;
+    }
 }
